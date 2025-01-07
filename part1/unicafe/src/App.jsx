0880ff3558270493be5db704502a715f97e8a58a
nom-gv <noemi.guarachi@gmail.com>
2025-01-07 17:01:18 -0400

    1.7 Unicafe, step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,15 +7,36 @@ const Button = ({ onClick, text }) => (
     {text}
   </button>
 )
+const Statistics = ({ good, neutral, bad, all, avarage, positive }) => {
+  return (
+    <p>
+      good {good} <br />
+      neutral {neutral} <br />
+      bad {bad} <br />
+      all {all} <br />
+      avarage {avarage} <br />
+      positive {positive} % <br />
+    </p>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const all = good + neutral + bad 
+  const avarage = (good - bad) / all
+  const positive = (good / all) * 100
+  
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <>
@@ -25,11 +46,14 @@ const App = () => {
       <Button onClick={handleBad} text='bad' />
 
       <Title text='statistics' />
-      <p>
-        good {good} <br />
-        neutral {neutral} <br />
-        bad {bad}
-      </p>
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        all={all} 
+        avarage={avarage} 
+        positive={positive}
+      />
     </>
   )
 }
