a1ce9116ec37ccfe709b0f553e874f950b23e6c2
nom-gv <noemi.guarachi@gmail.com>
2025-01-07 16:46:05 -0400

    1.6 Unicafe, step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,35 @@
+import { useState } from 'react'
+
+const Title = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
 
   return (
     <>
-      <h1>React 18</h1>
+      <Title text='give feedback' />
+      <Button onClick={handleGood} text='good' />
+      <Button onClick={handleNeutral} text='neutral' />
+      <Button onClick={handleBad} text='bad' />
+
+      <Title text='statistics' />
+      <p>
+        good {good} <br />
+        neutral {neutral} <br />
+        bad {bad}
+      </p>
     </>
   )
 }
