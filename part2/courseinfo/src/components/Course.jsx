a05ef3c7723255d3f02192d57fe383c55104e2bb
nom-gv <noemi.guarachi@gmail.com>
2026-01-27 22:16:36 -0400

    2.1 Courseinfom, step 6

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,26 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
+
+export default Course
