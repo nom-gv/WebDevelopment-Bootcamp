7726352579750a1bd9624f25e9adc0bd9eea2b01
nom-gv <noemi.guarachi@gmail.com>
2026-01-29 18:15:29 -0400

    2.2-3 Courseinfo, step 7 and 8

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -14,13 +14,19 @@ const Part = (props) => (
   </p>
 )
 
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p><b>total of {total} exercises</b></p>
+}
+
 const Course = (props) => {
   return (
     <div>
       <Header course={props.course.name} />
       <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
     </div>
   )
 }
 
-export default Course
+export default Course   
