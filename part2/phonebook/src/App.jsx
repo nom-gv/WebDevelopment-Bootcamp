a53c36926033888e4879fac79336917dfbf4915c
nom-gv <noemi.guarachi@gmail.com>
2026-01-29 20:32:15 -0400

    2.6 Phonebook, step 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import {Person} from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (    
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => 
+          <Person key={person.name} name={person.name} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/components/Person.jsx
@@ -0,0 +1,5 @@
+const Person = ({ name }) => {
+    return <p>{name}</p>
+}
+
+export { Person }
